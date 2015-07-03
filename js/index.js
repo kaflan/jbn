1a0014f5f38b01a7b08d8fd275b812c7109070ab
kaflan <kaf_90@mail.ru>
2015-07-03 13:21:05 +0300

    item status2

--- js/index.js
+++ js/index.js
@@ -11,26 +11,52 @@
   };
   var sortOrder;
   var data;
-  var app = angular.module('student', []);
-  // controller
-  //app.controller('', function(){});
-  // view
-  // model-view
+  var app = angular.module('jsbursa', []);
+
+  // model?? directive?
   app.directive('draggableList', function () {
     return {
       restrict: 'E',
-      template: ' <ul><li  ng-repeat="item in students" data-id="{{item.id}}" data-status="{{item.status}}"><h3>{{item.name}}</h3><h4>{{item.phone}}</h4></li></ul>'
-    }
+      scope: {
+        student: '='
+      },
+      replace: true,
+      template: ' <ul><li  data-id="{{student.id}}" data-status="{{student.status}}"><h3>{{student.name}}</h3><h4>{{student.phone}}</h4></li></ul>',
+      link: function ($scope, $element) {
+        $scope.$watch('students', function (newValue, oldValue) {
+          console.log('изменилось', newValue, oldValue, $element);
+        }, true);
+        $element.sortable({
+          placeholder: 'placeholder',
+          connectWith: $element.parent(),
+          receive: function (event, ui) {
+            if (ui.sender.parents('.removed').length) {
+//          ui.sender.sortable('cancel');
+            } else {
+              var newStatus = $(this).data('status');
+              var id = $(ui.item).data('id');
+              $.post(window.url + '/' + id, {status: newStatus}).error(function () {
+                ui.sender.sortable('cancel');
+              });
+            }
+          },
+          stop: function (event, ui) {
+
+          }
+
+        });
+        $scope.$applyAsync();
+      }
+    };
   });
   //controller  list student
-  app.controller('ListCtrlStud', function (scope, studentService, sortableService) {
+  app.controller('ListCtrlStud', function ($scope, studentService) {
     studentService.getStudent().success(function (newData) {
       console.log('success');
-      scope.students = newData;
+      $scope.students = newData;
     }).error(function () {
       console.log('fail');
     });
-    sortableService.getSort();
   });
   //  $applyAsync([exp]);
   app.service('studentService', function ($http) {
@@ -38,66 +64,28 @@
       return $http.get(window.url);
     };
   });
+
   //controller sort
-  app.service('sortableService', function() {
-    this.getSort = function () { return $('ul').sortable({
-      placeholder: 'placeholder',
-      connectWith: 'ul',
-      receive: function (event, ui) {
-        if (ui.sender.parents('.removed').length) {
-//          ui.sender.sortable('cancel');
-        } else {
-          var newStatus = $(this).data('status');
-          var id = $(ui.item).data('id');
-          $.post(window.url + '/' + id, {status: newStatus}).error(function () {
-            ui.sender.sortable('cancel');
-          });
-        }
-      },
-      stop: function (event, ui) {
 
-      }
 
-    });};
-  });
-  app.service('listSortService', function(scope){
-
-    this.listSort = function () {
-        scope.students.map(function(item){
-          if(item.status === ''){}
-        });
-      };
-
-  });
+  //app.service('listSortService', function(){
+  //
+  //  this.listSort = function () {
+  //    return {
+  //
+  //      }
+  //  };
+  //});
 
   // localstorage
   // save
   function Save() {
     localStorage.setItem('defaltSortOrder', JSON.stringify(defaltSortOrder));
   }
+
   // load
   function Load() {
     JSON.parse(localStorage.getItem('defaltSortOrder'));
   }
 
-
-
 })();
-//angular???
-
-//data.map(function it(item) {
-//  //template = $('<li><h3>' + item.name + '</h3><h4>' + item.phone + '</h4></li>');
-//  //template.attr({'data-id': item.id, 'data-status': item.status});
-//  if (item.status === 'removed') {
-//    //$removedUl.append(template);
-//    list.remove.push(item);
-//  }
-//  if (item.status === 'active') {
-//    //$activeUl.append(template);
-//    list.active.push(item);
-//  }
-//  if (item.status === 'redcard') {
-//    //$redCardUl.append(template);
-//    list.redcard.push(item);
-//  }
-//});
